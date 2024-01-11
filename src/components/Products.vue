e3d2902276c2e1c0b16ab3baa4b0b3a01c477299
Amir Lifshitz <amir1004@gmail.com>
2024-01-11 09:06:13 +0200

    initial commit

---
+++ src/components/Products.vue
@@ -0,0 +1,64 @@
+<template>
+  <v-row>
+    <Product v-for="(product, i) in products" :key="i" :product="product" />
+  </v-row>
+</template>
+
+<script>
+import Product from "./Product.vue";
+
+export default {
+  components: {
+    Product,
+  },
+  data: () => ({
+    products: [
+      {
+        img: " 3.jpg",
+        type: "Bedroom",
+        money: "450$",
+      },
+      {
+        img: "4.jpg",
+        type: "Furniture",
+        money: "600$",
+      },
+      {
+        img: "5.jpg",
+        type: "Home",
+        money: "430$",
+      },
+      {
+        img: "6.jpg",
+        type: "Interior",
+        money: "303$",
+      },
+      {
+        img: " 7.jpg",
+        type: "Living room",
+        money: "700$",
+      },
+      {
+        img: "8.jpg",
+        type: "Living room",
+        money: "360$",
+      },
+      {
+        img: "9.jpg",
+        type: "Living room",
+        money: "520$",
+      },
+      {
+        img: "10.jpg",
+        type: "Living room",
+        money: "360$",
+      },
+      {
+        img: "2.jpg",
+        type: "Living room",
+        money: "360$",
+      },
+    ],
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,30 +1,35 @@
 <template>
- <v-app>
-  <NavBar />
-  <v-main>
-    <v-container>
-      <v-card class="mar-top" flat>
-        <v-tabs v-model="tab" color="deep-purple-accent-4" align-tabs="center">
-        <v-tab :value="1">Newest</v-tab>
-        <v-tab :value="2">Highest Rated</v-tab>
-        <v-tab :value="3">Low Price to Hight</v-tab>
-        <v-tab :value="4">Low Price to Low</v-tab>
-        <v-tab :value="5">Delivery and Returns Products</v-tab>
-        </v-tabs>
-        <v-window v-model="tab">
-        <v-window-item v-for="n in 5" :key="n" :value="n">
-        <v-container fluid>
-          <v-row>
-            <v-col cols="12" sm="9">
-              <Product />
-            </v-col>
-            <v-col cols="12" sm="3">
-              <v-toolbar color="transparent">Categories
-                <v-spacer></v-spacer>
-                <v-btn icon="mdi-chevron-down"></v-btn>
-              </v-toolbar>
-              <v-row>
-                <v-col cols="12" sm="6" md="6">
+  <v-app>
+    <NavBar />
+    <v-main>
+      <v-container>
+        <v-card class="mar-top" flat>
+          <v-tabs
+            v-model="tab"
+            color="deep-purple-accent-4"
+            align-tabs="center"
+          >
+            <v-tab :value="1">Newest</v-tab>
+            <v-tab :value="2">Highest Rated</v-tab>
+            <v-tab :value="3">Low Price to Hight</v-tab>
+            <v-tab :value="4">Low Price to Low</v-tab>
+            <v-tab :value="5">Delivery and Returns Products</v-tab>
+          </v-tabs>
+          <v-window v-model="tab">
+            <v-window-item v-for="n in 5" :key="n" :value="n">
+              <v-container fluid>
+                <v-row>
+                  <v-col cols="12" sm="9">
+                    <Products />
+                  </v-col>
+                  <v-col cols="12" sm="3">
+                    <v-toolbar color="transparent"
+                      >Categories
+                      <v-spacer></v-spacer>
+                      <v-btn icon="mdi-chevron-down"></v-btn>
+                    </v-toolbar>
+                    <v-row>
+                      <v-col cols="12" sm="6" md="6">
                         <v-checkbox
                           v-model="ex4"
                           label="Sofas"
@@ -70,11 +75,12 @@
                           hide-details
                         ></v-checkbox>
                       </v-col>
-              </v-row>
-              <v-toolbar color="transparent">Colors<v-space></v-space>
-              <v-btn icon="mdi-chevron-down"></v-btn>
-              </v-toolbar>
-              <v-avatar color="grey" class="ml-1"></v-avatar>
+                    </v-row>
+                    <v-toolbar color="transparent"
+                      >Colors<v-space></v-space>
+                      <v-btn icon="mdi-chevron-down"></v-btn>
+                    </v-toolbar>
+                    <v-avatar color="grey" class="ml-1"></v-avatar>
                     <v-avatar color="#DBEBCE" class="ml-1"></v-avatar>
                     <v-avatar color="#EBDFD1" class="ml-1"></v-avatar>
                     <v-avatar color="#E6C5B8" class="ml-1"></v-avatar>
@@ -86,8 +92,9 @@
                     <v-avatar color="#8B9DC2" class="ml-1 mt-3"></v-avatar>
                     <v-avatar color="#959DB6" class="ml-1 mt-3"></v-avatar>
                     <v-avatar color="#CFE7EB" class="ml-1 mt-3"></v-avatar>
-                    <v-toolbar color="transparent">Materials <v-spacer></v-spacer>
-                    <v-btn icon="mdi-chevron-down"></v-btn>
+                    <v-toolbar color="transparent"
+                      >Materials <v-spacer></v-spacer>
+                      <v-btn icon="mdi-chevron-down"></v-btn>
                     </v-toolbar>
                     <v-row>
                       <v-col cols="12" sm="6" md="6">
@@ -137,31 +144,37 @@
                         ></v-checkbox>
                       </v-col>
                     </v-row>
-                    <v-toolbar color="transparent">Store Ratings<v-spacer></v-spacer>
-                    <v-btn icon="mdi-chevron-down"></v-btn>
+                    <v-toolbar color="transparent"
+                      >Store Ratings<v-spacer></v-spacer>
+                      <v-btn icon="mdi-chevron-down"></v-btn>
                     </v-toolbar>
                     <v-rating v-model="r1" color="yellow-darken-3"></v-rating>
                     <v-rating v-model="r2" color="yellow-darken-3"></v-rating>
                     <v-rating v-model="r3" color="yellow-darken-3"></v-rating>
-                    <v-toolbar color="transparent">Price<v-spacer></v-spacer>
+                    <v-toolbar color="transparent"
+                      >Price<v-spacer></v-spacer>
                       <v-btn icon="mdi-chevron-down"></v-btn>
                     </v-toolbar>
-                    <v-slider v-model="slider2" thumb-label="always" color="pink"></v-slider>
-            </v-col>
-          </v-row>
-        </v-container>
-        </v-window-item>
-        </v-window>
-      </v-card>
-    </v-container>
-  </v-main>
-  <Footer />
- </v-app>
+                    <v-slider
+                      v-model="slider2"
+                      thumb-label="always"
+                      color="pink"
+                    ></v-slider>
+                  </v-col>
+                </v-row>
+              </v-container>
+            </v-window-item>
+          </v-window>
+        </v-card>
+      </v-container>
+    </v-main>
+    <Footer />
+  </v-app>
 </template>
 
 <script setup>
 import NavBar from "@/components/NavBar.vue";
-import Product from "@/components/Product.vue";
+import Products from "@/components/Products.vue";
 import Footer from "@/components/Footer.vue";
 </script>
 <script>
@@ -209,4 +222,3 @@ export default {
   margin-top: 250px;
 }
 </style>
-
