078357ef6a97d3772f70059a2e1453432a5386d5
Amir Lifshitz <amir1004@gmail.com>
2024-01-11 13:04:42 +0200

    cache6

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,6 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "vuejs-vuetify-furniture-web-site",
   plugins: [
     vue({
       template: { transformAssetUrls },
